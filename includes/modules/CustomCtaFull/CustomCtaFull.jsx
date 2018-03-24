1ba14ea6978b8d983635600372f5e24ee25d8cb9
Fikri Rasyid <fikrirasyid@gmail.com>
2018-03-24 11:29:55 +0700

    Sample plugins :: fixed VB component for CTA with full VB support

--- includes/modules/CustomCtaFull/CustomCtaFull.jsx
+++ includes/modules/CustomCtaFull/CustomCtaFull.jsx
@@ -6,45 +6,38 @@ import './style.css';
 
 
 class CustomCtaFull extends Component {
-  hasValue( value ) {
-    return '' !== value && typeof value !== 'undefined' && value !== false;
-  }
-
-  // WIP
-  processFontIcon() {
-
-  }
-
-  // WIP, copied from cta.jsx
-  // @todo either replicate utils as method, or find a way to import utils
   renderButton() {
-    return '';
-    // const attrs = this.props;
-    // const buttonTarget = 'on' === attrs.url_new_window ? '_blank' : '';
-    // const isCustomButtonIcon = this.hasValue(attrs.button_icon);
-    // const buttonIcon = isCustomButtonIcon ? Utils.processFontIcon(attrs.button_icon) : false;
-    // const buttonClassName = {
-    //   et_pb_promo_button: true,
-    //   et_pb_button: true,
-    //   et_pb_custom_button_icon: isCustomButtonIcon,
-    // };
+    const attrs = this.props;
+    const utils = this.props.utils;
+    const buttonTarget = 'on' === attrs.url_new_window ? '_blank' : '';
+    const isCustomButtonIcon = utils.hasValue(attrs.button_icon);
+    const buttonIcon = isCustomButtonIcon ? utils.processFontIcon(attrs.button_icon) : false;
+    const buttonClassName = {
+      et_pb_promo_button: true,
+      et_pb_button: true,
+      et_pb_custom_button_icon: isCustomButtonIcon,
+    };
 
-    // return ! this.hasValue(attrs.button_text) || ! this.hasValue(attrs.button_url) ? '' : (
-    //   <div className='et_pb_button_wrapper'><a
-    //     className={classnames(buttonClassName)}
-    //     href={attrs.button_url}
-    //     target={buttonTarget}
-    //     rel={this.linkRel('button')}
-    //     data-icon={buttonIcon}
-    //   >{attrs.button_text}</a></div>
-    // );
+    return ! utils.hasValue(attrs.button_text) || ! utils.hasValue(attrs.button_url) ? '' : (
+      <div className='et_pb_button_wrapper'><a
+        className={utils.classnames(buttonClassName)}
+        href={attrs.button_url}
+        target={buttonTarget}
+        rel={utils.linkRel(attrs.button_rel)}
+        data-icon={buttonIcon}
+      >{attrs.button_text}</a></div>
+    );
   }
 
+  /**
+   * Module render in VB
+   * Basically DICM_CTA_Has_VB_Support->render() equivalent in JSX
+   */
   render() {
     return (
       <div>
         <h2 className="dicm-title">{this.props.title}</h2>
-        <div className="dicm-content">{this.props.content}</div>
+        <div className="dicm-content">{this.props.content()}</div>
         {this.renderButton()}
       </div>
     );
