07a1cdb74398c24423c0b324928d863cc42f61e6
Fikri Rasyid <fikrirasyid@gmail.com>
2018-03-30 21:56:37 +0700

    Sample plugins :: used property instead of method for builder & frontend js data
    Addressing @lots0logs' feedback at
    https://github.com/elegantthemes/submodule-builder/pull/3066#discussion_
    r178295542

--- includes/DiviCustomModules.php
+++ includes/DiviCustomModules.php
@@ -37,20 +37,7 @@ class DICM_DiviCustomModules extends DiviExtension {
 	public function __construct( $name = 'divi-custom-modules', $args = array() ) {
 		$this->plugin_dir              = plugin_dir_path( __FILE__ );
 		$this->plugin_dir_url          = plugin_dir_url( $this->plugin_dir );
-
-		parent::__construct( $name, $args );
-	}
-
-	/**
-	 * Provide localized strings for Visual Builder component. These array will automatically enqueued
-	 * for Visual Builder component script and can be accessed using `window.DiviCustomModulesBuilderData` variable
-	 *
-	 * @since 1.0.0
-	 *
-	 * return array()
-	 */
-	protected function builder_js_data() {
-		return array(
+		$this->_builder_js_data        = array(
 			'i10n' => array(
 				'dicm_cta_all_options' => array(
 					'basic_fields'         => esc_html__( 'Basic Fields', 'dicm-divi-custom-modules' ),
@@ -91,6 +78,8 @@ class DICM_DiviCustomModules extends DiviExtension {
 				),
 			),
 		);
+
+		parent::__construct( $name, $args );
 	}
 }
 
